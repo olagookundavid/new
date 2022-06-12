60709e0801e4d4d2cf1862dee031a635a22a121d
david oh <erijesudo@gmail.com>
2022-06-12 12:22:48 +0100

    fixed error ScrollController is currently attached to morethan one ScrollPosition

---
+++ lib/screens/mobile_screen_layout.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/widgets/contacts_list.dart';
+
+class MobileScreenLayout extends StatelessWidget {
+  const MobileScreenLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // for tabs, we declared the tab bar later on
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Whatsapp',
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          elevation: 0,
+          backgroundColor: appBarColor,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.more_vert,
+                color: Colors.grey,
+              ),
+            ),
+          ],
+          //bottom used to create a widget bellow the appbar, as per doc usually tabbar
+          bottom: const TabBar(
+            indicatorColor: tabColor,
+            labelColor: tabColor,
+            unselectedLabelColor: Colors.grey,
+            labelStyle: TextStyle(fontWeight: FontWeight.bold),
+            indicatorWeight: 4,
+            tabs: [
+              Tab(text: 'Chats'),
+              Tab(text: 'Status'),
+              Tab(text: 'Calls'),
+            ],
+          ),
+        ),
+        body: const ContactsList(),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/contacts_list.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
+import '../info.dart';
+
+class ContactsList extends StatelessWidget {
+  const ContactsList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: info.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text(info[index]['name'].toString(),
+                      style: const TextStyle(fontSize: 15)),
+                  subtitle: Padding(
+                    padding: const EdgeInsets.only(top: 6),
+                    child: Text(
+                      info[index]['message'].toString(),
+                      style: const TextStyle(fontSize: 15),
+                    ),
+                  ),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      info[index]['profilePic'].toString(),
+                    ),
+                    radius: 30,
+                  ),
+                  trailing: Text(
+                    info[index]['time'].toString(),
+                    style: const TextStyle(fontSize: 13, color: Colors.grey),
+                  ),
+                ),
+              ),
+              const Divider(
+                color: dividerColor,
+                indent: 85,
+              )
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
